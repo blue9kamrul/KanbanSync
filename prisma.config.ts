470ea3613d1c92adac912c2f8fffa157730fd8e4
kamrul <cdkamrul9@gmail.com>
2026-02-27 23:28:50 +0600

    Built DB shcema and setup .env file. Pushed to DB and generated the type safety files. Added prisma adpter and pg with types. Added new prisma client with adapter.

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,10 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  schema: "./prisma/schema.prisma",
+  // migrations: {
+  //   path: "prisma/migrations",
+  // },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
