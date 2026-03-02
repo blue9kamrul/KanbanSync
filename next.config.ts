9c0a96a107420326c497c3a8da3e6ac2a6db8cdc
kamrul <cdkamrul9@gmail.com>
2026-03-02 17:50:41 +0600

    Fixed nextconfig file to eliminate deployment error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add this block to fix the Vercel internal output error!
+  serverExternalPackages: ['pg', '@prisma/client', '@auth/prisma-adapter'],
+
+  // (Keep any other config you already had here)
 };
 
 export default nextConfig;
